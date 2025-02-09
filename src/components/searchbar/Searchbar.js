3714a85c42c6e9891da6a0e149a167062b80a122
Jordan <jordan.donguy@gmail.com>
2025-02-09 19:21:52 +0100

    Add an onClick function to the searchbar

--- src/components/searchbar/Searchbar.js
+++ src/components/searchbar/Searchbar.js
@@ -1,16 +1,23 @@
 import React, {useState} from 'react';
 import styles from './Searchbar.module.css';
+import { useDispatch } from 'react-redux';
+import { loadingPosts } from '../../app/slices/postsSlice';
 
 const Searchbar = () => {
     const [searchTerm, setSearchTerm] = useState('');
+    const dispatch = useDispatch();
 
     function handleOnChange(e) {
         setSearchTerm(e.currentTarget.value)
     }
 
+    function handleOnClick(e) {
+      dispatch(loadingPosts(searchTerm))
+    }
+
   return (
     <div className={styles.searchbar}>
-        <button className={styles.button}></button>
+        <button className={styles.button} onClick={handleOnClick}></button>
         <input
         type='text'
         value={searchTerm}
